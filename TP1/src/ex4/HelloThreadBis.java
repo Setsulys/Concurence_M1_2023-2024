6f6ea7de7971e65d8bb8fbbb12189a1eda278fe0
LY-IENG Steven <steven.ly412@gmail.com>
2023-09-20 12:06:20 +0200

    Tp1

--- TP1/src/ex4/HelloThreadBis.java
+++ TP1/src/ex4/HelloThreadBis.java
@@ -1,30 +1,25 @@
 package ex4;
 
-import ex1.HelloThread;
-
 public class HelloThreadBis {
 
-	public static void println(String s){
-	  for(var i = 0; i < s.length(); i++){
-	    System.out.print(s.charAt(i));
-	  }
-	  System.out.print("\n");
-	}
-	
-	@SuppressWarnings("preview")
-	public void newThread(int value) {
-		Thread.ofPlatform().name("Hello " + value + " ").start(()->{
-			for(var i=0;i < 5000;i++) {
-				var str = Thread.currentThread().getName() + i;
-				HelloThreadBis.println(str);
-			}
-		});
-	}
-	
-	public static void main(String[] args) {
-		var th = new HelloThread();
-		for(var i=0;i<4;i++) {
-			th.newThread(i);
-		}
-	}
+  public static void println(String s) {
+    for (var i = 0; i < s.length(); i++) {
+      System.out.print(s.charAt(i));
+    }
+    System.out.print("\n");
+  }
+
+  @SuppressWarnings("preview")
+public static void main(String[] args) {
+    int nbThreads = 5;
+
+    for (int j = 0; j < nbThreads; j++) {
+      int actuallyFinal = j;
+      Thread.ofPlatform().start(() -> {
+        for (int i = 0; i < 5000; i++) {
+          println("hello " + actuallyFinal + " " + i);
+        }
+      });
+    }
+  }
 }
---
+++ TP1/src/ex4/HelloThreadBis2.java
@@ -0,0 +1,30 @@
+package ex4;
+
+import ex1.HelloThread;
+
+public class HelloThreadBis2 {
+
+	public static void println(String s){
+	  for(var i = 0; i < s.length(); i++){
+	    System.out.print(s.charAt(i));
+	  }
+	  System.out.print("\n");
+	}
+	
+	@SuppressWarnings("preview")
+	public void newThread(int value) {
+		Thread.ofPlatform().name("Hello " + value + " ").start(()->{
+			for(var i=0;i < 5000;i++) {
+				var str = Thread.currentThread().getName() + i;
+				HelloThreadBis2.println(str);
+			}
+		});
+	}
+	
+	public static void main(String[] args) {
+		var th = new HelloThread();
+		for(var i=0;i<4;i++) {
+			th.newThread(i);
+		}
+	}
+}
