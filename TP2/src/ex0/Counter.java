e3b22fb22d4fa043bc4d298dcc0f42cb7db02c18
LY-IENG Steven <steven.ly412@gmail.com>
2023-10-02 20:21:33 +0200

    TP2 modified

--- TP2/src/ex0/Counter.java
+++ TP2/src/ex0/Counter.java
@@ -23,8 +23,11 @@ public class Counter {
  * A chaque execution de la classe, on obtient une valeur différente qui sera toujours superieur a 100_000
  * 
  * 3 Est-il possible que ce code affiche moins que 100 000 ? Expliquer précisément pourquoi.
- * Non il n'est pas possible que le code affiche moins que cette valeur. En effet, il y a plusieurs threads qui s'incrémentent jusqu'a 100_000.
- * Cependant, la ligne ``this.value++`` n'est pas atomique, en bytecode, elle sera traduite en plusieurs lignes.
+ * Oui, c'est possible mais c'est très rare. En effet, il y a plusieurs threads qui s'incrémentent jusqu'a 100_000.
+ * Cependant, la ligne ``this.value++`` n'est pas atomique, en bytecode, elle sera traduite en plusieurs lignes
  * Comme il y a plusieurs threads qui sont en écriture, il peut y avoir des effets de bords.
+ * Il se peut que l'un des threads s'execute jusqu'a avant de l'ajouter puis se bloque
+ * Le deuxieme thread se lance et s'execute entierement jusqu'a 9_999 et se stope mais le thread 1 reprend la main et donc ecrase les 9_999
+ * puis de meme pour le thread 2 
  */
 
