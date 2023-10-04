9611022bf137f107e4b8445b5d2dc218d42732ed
LY-IENG Steven <steven.ly412@gmail.com>
2023-10-04 12:52:00 +0200

    TP3

---
+++ TP3/src/ex4/RendezVous.java
@@ -0,0 +1,42 @@
+package ex4;
+
+import java.util.Objects;
+
+/**
+ * Note: this code does several stupid things !
+ */
+public class RendezVous<V> {
+  private V value;
+  private final Object lock = new Object();
+  
+  public void set(V value) {
+    Objects.requireNonNull(value);
+    synchronized(lock) {
+    	this.value = value;
+    }
+  }
+  
+  public V get() throws InterruptedException {
+	synchronized(lock) {
+		while(value == null) {
+        	//Thread.sleep(1);  // then comment this line !
+    	}
+		return value;
+	}
+	
+  }
+  
+  public static void main(String[] args) throws InterruptedException {
+    RendezVous<String> rendezVous = new RendezVous<>();
+    Thread.ofPlatform().start(() -> {
+      try {
+        Thread.sleep(5000);
+      } catch (InterruptedException e) {
+        throw new AssertionError(e);
+      }
+      rendezVous.set("hello");
+    });
+    
+    System.out.println(rendezVous.get());
+  }
+}
